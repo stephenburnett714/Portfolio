f57e6db01b9bbc0881e23ddc2dbe1e73f11e4dff
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-04-01 16:56:54 -0400

    changed github link to the correct link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ export default function NavBar(props) {
             </div>
             <div className="nav-inner-container">
             <span className="nav-text">Stephen Burnett</span>
-            <span className="nav-span"><a href="https://www.linkedin.com/in/stephen-burnett" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className="footer-logo fa-2x" icon={faGithub} /></a></span>
+            <span className="nav-span"><a href="https://github.com/stephenburnett714/" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className="footer-logo fa-2x" icon={faGithub} /></a></span>
             <span className="nav-span"><a href="https://www.linkedin.com/in/stephen-burnett" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className="footer-logo fa-2x" icon={faLinkedin} /></a></span>
             </div>
         </div>
